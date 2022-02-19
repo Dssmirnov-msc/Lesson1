52588b0df4aaa7269953f1e8dba03d91cb4e66a6
Dssmirnov-msc <61143611+Dssmirnov-msc@users.noreply.github.com>
2022-02-19 11:24:24 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -16,6 +16,7 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.println("Здесь был Вася");
+        System.out.println("Рыбы не мы, мы не рыбы");
     }
     
 }
